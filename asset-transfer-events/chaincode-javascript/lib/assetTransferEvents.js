2f85b1256fb2c50aba98dcc80822110130e7fe0f
Edinei da Motta Martinello <96083750+edineim@users.noreply.github.com>
2023-11-24 21:39:17 -0300

    Update assetTransferEvents.js

--- asset-transfer-events/chaincode-javascript/lib/assetTransferEvents.js
+++ asset-transfer-events/chaincode-javascript/lib/assetTransferEvents.js
@@ -64,24 +64,6 @@ async function readState(ctx, id) {
 
 class AssetTransferEvents extends Contract {
 
-	// CreateAsset issues a new asset to the world state with given details.
-	// async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
-	// 	const asset = {
-	// 		ID: id,
-	// 		Color: color,
-	// 		Size: size,
-	// 		Owner: owner,
-	// 		AppraisedValue: appraisedValue,
-	// 	};
-	// 	await savePrivateData(ctx, id);
-	// 	const assetBuffer = Buffer.from(JSON.stringify(asset));
-
-	// 	ctx.stub.setEvent('CreateAsset', assetBuffer);
-	// 	return ctx.stub.putState(id, assetBuffer);
-	// }
-
-	// CreateAsset
-	// Mudando a ordem da struct
 	async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
 		const asset = {
 			ID: id,
@@ -97,8 +79,6 @@ class AssetTransferEvents extends Contract {
 		return ctx.stub.putState(id, assetBuffer);
 	}
 
-	// TransferAsset updates the owner field of an asset with the given id in
-	// the world state.
 	async TransferAssetDistribuidor(ctx, id, newOwner, color) {
 		const asset = await readState(ctx, id);
 		asset.Owner = newOwner;
@@ -121,7 +101,6 @@ class AssetTransferEvents extends Contract {
 		return ctx.stub.putState(id, assetBuffer);
 	}
 
-	// ReadAsset returns the asset stored in the world state with given id.
 	async ReadAsset(ctx, id) {
 		const asset = await readState(ctx, id);
 		await addPrivateData(ctx, asset.ID, asset);
@@ -129,7 +108,6 @@ class AssetTransferEvents extends Contract {
 		return JSON.stringify(asset);
 	}
 
-	// UpdateAsset updates an existing asset in the world state with provided parameters.
 	async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
 		const asset = await readState(ctx, id);
 		asset.Color = color;
@@ -143,7 +121,6 @@ class AssetTransferEvents extends Contract {
 		return ctx.stub.putState(id, assetBuffer);
 	}
 
-	// DeleteAsset deletes an given asset from the world state.
 	async DeleteAsset(ctx, id) {
 		const asset = await readState(ctx, id);
 		const assetBuffer = Buffer.from(JSON.stringify(asset));
@@ -170,187 +147,6 @@ class AssetTransferEvents extends Contract {
 			return asset;
 		}	        
     }
-
-	// async CreateGenesisBlock(ctx, viticultor, enderecoViticultor, variedadeUva, dataColheita) {
-	// 	const genesisBlock = {
-	// 		Viticultor: viticultor,
-	// 		EnderecoViticultor: enderecoViticultor,
-	// 		VariedadeUva: variedadeUva,
-	// 		DataColheita: dataColheita,
-	// 	};
-	
-	// 	const genesisBlockBuffer = Buffer.from(JSON.stringify(genesisBlock));
-	
-	// 	ctx.stub.setEvent('CreateGenesisBlock', genesisBlockBuffer);
-	// 	return ctx.stub.putState('genesis', genesisBlockBuffer);
-	// }
-
-	// async TransferToDistributor(ctx, idRemessa, nomeProdutorDistribuidor, enderecoProdutorDistribuidor, lote, dataEmbarque, horaEmbarque) {
-	// 	const block = await readState(ctx, 'genesis');
-	
-	// 	const transaction = {
-	// 		IDRemessa: idRemessa,
-	// 		NomeProdutorDistribuidor: nomeProdutorDistribuidor,
-	// 		EnderecoProdutorDistribuidor: enderecoProdutorDistribuidor,
-	// 		Lote: lote,
-	// 		DataEmbarque: dataEmbarque,
-	// 		HoraEmbarque: horaEmbarque,
-	// 	};
-	
-	// 	block.Transactions.push(transaction);
-	
-	// 	const blockBuffer = Buffer.from(JSON.stringify(block));
-	
-	// 	ctx.stub.setEvent('TransferToDistributor', blockBuffer);
-	// 	return ctx.stub.putState('genesis', blockBuffer);
-	// }
-	
-	// async TransferToRetailer(ctx, idRemessa, nomeDistribuidor, endereco, dataEmbarque, horaEmbarque) {
-	// 	const block = await readState(ctx, 'genesis');
-	
-	// 	const transaction = {
-	// 		IDRemessa: idRemessa,
-	// 		NomeDistribuidor: nomeDistribuidor,
-	// 		Endereco: endereco,
-	// 		DataEmbarque: dataEmbarque,
-	// 		HoraEmbarque: horaEmbarque,
-	// 	};
-	
-	// 	block.Transactions.push(transaction);
-	
-	// 	const blockBuffer = Buffer.from(JSON.stringify(block));
-	
-	// 	ctx.stub.setEvent('TransferToRetailer', blockBuffer);
-	// 	return ctx.stub.putState('genesis', blockBuffer);
-	// }
 }
 
 module.exports = AssetTransferEvents;
-
-
-// 'use strict';
-
-// const { Contract } = require('fabric-contract-api');
-
-// async function savePrivateData(ctx, assetKey, previousBlockID) {
-//     const clientOrg = ctx.clientIdentity.getMSPID();
-//     const peerOrg = ctx.stub.getMspID();
-//     const collection = '_implicit_org_' + peerOrg;
-
-//     const data = {
-//         previousBlockID: previousBlockID
-//     };
-
-//     if (clientOrg === peerOrg) {
-//         const transientMap = ctx.stub.getTransient();
-//         if (transientMap) {
-//             const properties = transientMap.get('asset_properties');
-//             if (properties) {
-//                 data.asset_properties = properties;
-//             }
-//         }
-//     }
-
-//     await ctx.stub.putPrivateData(collection, assetKey, Buffer.from(JSON.stringify(data)));
-// }
-
-// async function removePrivateData(ctx, assetKey) {
-//     const clientOrg = ctx.clientIdentity.getMSPID();
-//     const peerOrg = ctx.stub.getMspID();
-//     const collection = '_implicit_org_' + peerOrg;
-
-//     if (clientOrg === peerOrg) {
-//         const dataBuffer = await ctx.stub.getPrivateData(collection, assetKey);
-//         if (dataBuffer && dataBuffer.length > 0) {
-//             await ctx.stub.deletePrivateData(collection, assetKey);
-//         }
-//     }
-// }
-
-// async function addPrivateData(ctx, assetKey, asset) {
-//     const clientOrg = ctx.clientIdentity.getMSPID();
-//     const peerOrg = ctx.stub.getMspID();
-//     const collection = '_implicit_org_' + peerOrg;
-
-//     if (clientOrg === peerOrg) {
-//         const dataBuffer = await ctx.stub.getPrivateData(collection, assetKey);
-//         if (dataBuffer && dataBuffer.length > 0) {
-//             const data = JSON.parse(dataBuffer.toString());
-//             if (data.asset_properties) {
-//                 asset.asset_properties = data.asset_properties;
-//             }
-//             asset.previousBlockID = data.previousBlockID;
-//         }
-//     }
-// }
-
-// async function readState(ctx, id) {
-//     const assetBuffer = await ctx.stub.getState(id);
-//     if (!assetBuffer || assetBuffer.length === 0) {
-//         throw new Error(`The asset ${id} does not exist`);
-//     }
-//     const assetString = assetBuffer.toString();
-//     const asset = JSON.parse(assetString);
-
-//     return asset;
-// }
-
-// class AssetTransferEvents extends Contract {
-
-//     async CreateAsset(ctx, id, color, size, owner, appraisedValue, previousBlockID) {
-//         const asset = {
-//             ID: id,
-//             Color: color,
-//             Size: size,
-//             Owner: owner,
-//             AppraisedValue: appraisedValue,
-//         };
-//         await savePrivateData(ctx, id, previousBlockID);
-//         const assetBuffer = Buffer.from(JSON.stringify(asset));
-
-//         ctx.stub.setEvent('CreateAsset', assetBuffer);
-//         return ctx.stub.putState(id, assetBuffer);
-//     }
-
-//     async TransferAsset(ctx, id, newOwner, previousBlockID) {
-//         const asset = await readState(ctx, id);
-//         asset.Owner = newOwner;
-//         const assetBuffer = Buffer.from(JSON.stringify(asset));
-//         await savePrivateData(ctx, id, previousBlockID);
-
-//         ctx.stub.setEvent('TransferAsset', assetBuffer);
-//         return ctx.stub.putState(id, assetBuffer);
-//     }
-
-//     async ReadAsset(ctx, id) {
-//         const asset = await readState(ctx, id);
-//         await addPrivateData(ctx, asset.ID, asset);
-
-//         return JSON.stringify(asset);
-//     }
-
-//     async UpdateAsset(ctx, id, color, size, owner, appraisedValue, previousBlockID) {
-//         const asset = await readState(ctx, id);
-//         asset.Color = color;
-//         asset.Size = size;
-//         asset.Owner = owner;
-//         asset.AppraisedValue = appraisedValue;
-//         const assetBuffer = Buffer.from(JSON.stringify(asset));
-//         await savePrivateData(ctx, id, previousBlockID);
-
-//         ctx.stub.setEvent('UpdateAsset', assetBuffer);
-//         return ctx.stub.putState(id, assetBuffer);
-//     }
-
-//     async DeleteAsset(ctx, id, previousBlockID) {
-//         const asset = await readState(ctx, id);
-//         const assetBuffer = Buffer.from(JSON.stringify(asset));
-//         await removePrivateData(ctx, id);
-//         await savePrivateData(ctx, id, previousBlockID);
-
-//         ctx.stub.setEvent('DeleteAsset', assetBuffer);
-//         return ctx.stub.deleteState(id);
-//     }
-// }
-
-// module.exports = AssetTransferEvents;
